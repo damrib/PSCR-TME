08f71f8b233c7af12baba6cb5e11fb1875b88c72
damrib <damienribeiro804@gmail.com>
2024-10-09 12:17:53 +0200

    Rendu initial TME4

--- TME4/src/Banque.cpp
+++ TME4/src/Banque.cpp
@@ -7,19 +7,41 @@ using namespace std;
 namespace pr {
 
 void Banque::transfert(size_t deb, size_t cred, unsigned int val) {
+	//std::lock_guard<std::mutex> lok(m);
+
 	Compte & debiteur = comptes[deb];
 	Compte & crediteur = comptes[cred];
+
+	if (deb > cred) {
+		crediteur.lock();
+		debiteur.lock();
+	} else {
+		debiteur.lock();
+		crediteur.lock();
+	}
+
 	if (debiteur.debiter(val)) {
 		crediteur.crediter(val);
+	}
+
+	if ( deb > cred ){
+		crediteur.unlock();
+		debiteur.unlock();
+	} else {
+		debiteur.unlock();
+		crediteur.unlock();
 	}
 }
 size_t Banque::size() const {
 	return comptes.size();
 }
 bool Banque::comptabiliser (int attendu) const {
+	//std::lock_guard<std::mutex> lok(m);
 	int bilan = 0;
 	int id = 0;
+
 	for (const auto & compte : comptes) {
+		compte.lock();
 		if (compte.getSolde() < 0) {
 			cout << "Compte " << id << " en négatif : " << compte.getSolde() << endl;
 		}
@@ -29,6 +51,11 @@ bool Banque::comptabiliser (int attendu) const {
 	if (bilan != attendu) {
 		cout << "Bilan comptable faux : attendu " << attendu << " obtenu : " << bilan << endl;
 	}
+
+	for (const auto& c : comptes) {
+		c.unlock();
+	}
+
 	return bilan == attendu;
 }
 }
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,16 +1,62 @@
 #include "Banque.h"
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
 
-using namespace std;
+void random_transac_then_sleep(pr::Banque& b) {
+	size_t id_account1 = std::rand() % b.size();
+	size_t id_account2;
+	do {
+		id_account2 = std::rand() % b.size();
+	} while (id_account1 == id_account2);
+	unsigned int v = std::rand() % 100;
+	for (int i = 0 ; i < 1000; ++i){
+		b.transfert(id_account1, id_account2, v);
+	}
+	auto sl_t = std::chrono::milliseconds((std::rand() % 20));
+	std::this_thread::sleep_for(sl_t);
+}
+
+void thread_comptabilisation(pr::Banque& b, int expected, bool* res) {
+
+	for (int i = 0; i < 1000; ++i) {
+		*res = *res && b.comptabiliser(expected);
+		return;
+	}
+
+}
+
+/**int threadRandom(int min, int max) { // max inclusif
+	static thread_local std::mt19934 gem;
+	std::uniform_int_distribution<int> d(min, max);
+	return d(gem);
+}**/
 
 const int NB_THREAD = 10;
 int main () {
-	vector<thread> threads;
-	// TODO : creer des threads qui font ce qui est demandé
+	std::srand(std::time(nullptr));
 
+	std::vector<std::thread> threads;
+	// TODO : creer des threads qui font ce qui est demandé
+	threads.reserve(NB_THREAD);
+
+	size_t nb_account = NB_THREAD / 4;
+	pr::Banque b(nb_account, 100);
+
+	for ( int i = 0; i < NB_THREAD; ++i) {
+		threads.emplace_back(random_transac_then_sleep, std::ref(b));
+	}
+
+	bool invariant = true;
+	std::thread comptable(thread_comptabilisation, std::ref(b), 100 * b.size(), &invariant);
 
 	for (auto & t : threads) {
 		t.join();
 	}
+	comptable.join();
+
+	std::cout << "l'invariant est-il maintenu le long de l'execution? " << invariant << std::endl;
+	std::cout << b.comptabiliser(100 * b.size()) << std::endl; 
 
 	// TODO : tester solde = NB_THREAD * JP
 	return 0;
