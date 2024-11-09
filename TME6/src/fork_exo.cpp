49bccb39342b86a88df3ace369fb18e76177854b
damrib <damienribeiro804@gmail.com>
2024-11-09 09:50:02 +0100

    TME6

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
+
 
 int main () {
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
 
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
-		std::cout << " i:j " << i << ":" << j << std::endl;
-		for (int k=1; k<=i && j==N ; k++) {
+	int cpt = 0;
+	for (int i=1, j=N; i<=N && j==N && ++cpt && fork()==0 ; i++ ) { // main ne cree qu'un seul fils
+		cpt = 0;
+		std::cout << " i:j " << i << ":" << j << std::endl; // i 
+		for (int k=1; k<=i && j==N ; k++) { // Seul les fils du main peuvent creer des fils (3eme fils main -> 3fils, 2nd fils main -> 2fils, 1ee fils main -> 1fils)
 			if ( fork() == 0) {
-				j=0;
+				j=0; // Les fils de fils ont J a 0
+				cpt = 0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
-			}
+			} else ++cpt;
 		}
 	}
+
+	while (cpt-- > 0) {
+		int status;
+		wait(&status);
+	}
+
 	return 0;
 }
+
+// Arbre des processus créés par le programme
+// main -> i:j 1:3 -> i:j 2:3  -> i:j 3:3 -> k:j 1:0
+//                 -> k:j 1:0 |-> k:j 1:0 -> k:j 2:0
+//         	                   -> k:j 2:0 |-> k:j 3:0
+//                 
+             
--- TME6/src/rsleep.cpp
+++ TME6/src/rsleep.cpp
@@ -1,6 +1,70 @@
 #include <ctime>
 #include <cstdlib>
+#include <cstring>
+#include <signal.h>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "rsleep.h"
+
+int proc_hp = 5;
+std::string name;
+
+void FatherWins(int sig){
+  exit(0);
+}
+
+void handler(int sig) {
+  std::cout << name << ":" << proc_hp << std::endl;
+	if ( --proc_hp == 0 ){
+		if ( name == "Vader" ) { 
+      std::cout << "NOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!" << std::endl;
+    } else if ( name == "Luke") {
+      std::cout << "No...that's not true, that's impossible...NOOOOOO!!!!!!!!" << std::endl;
+    }
+    exit(1);
+	}
+  signal(SIGINT, &handler);
+}
+
+void pary(int sig) {
+  std::cout << "coup paré " << std::endl;
+}
+
+void attaque(pid_t adversaire) {
+  /**struct sigaction act;
+  sigfillset(&act.sa_mask);
+  act.sa_flags = 0;
+  act.sa_handler = &handler;
+  sigaction(SIGINT, &act, NULL);**/
+  signal(SIGINT, &handler);
+  kill(adversaire, SIGINT);
+  randsleep();
+}
+
+void defense() {
+  signal(SIGINT, SIG_IGN);
+  /**if (name == "Vader") signal(SIGINT, SIG_IGN);
+  else {
+    signal(SIGINT, &pary);
+    sigset_t setPos;
+    sigemptyset(&setPos);
+    sigaddset(&setPos, SIGINT);
+    sigprocmask(SIG_BLOCK, &setPos, NULL);
+    randsleep();
+    sigsuspend(&setPos);
+  }**/
+  randsleep();
+}
+
+void combat(pid_t adversaire) {
+
+  while ( true ) {
+    defense();
+    attaque(adversaire);
+  }
+
+}
 
 void randsleep() {
   int r = rand();
@@ -13,4 +77,21 @@ void randsleep() {
   while ( nanosleep(&tosleep, &remain) != 0) {
     tosleep = remain;
   }
+}
+
+int main() {
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    srand(time(NULL));
+    name = "Luke";
+    proc_hp = 1;
+    combat(getppid());
+  } else {
+    srand(time(NULL));
+    name = "Vader";
+    signal(SIGCHLD, &FatherWins);
+    combat(pid);
+  }
+
 }
